7774957a73fa7e547808671f27e34c726dcbbb4f
Emmanuel Boersma <emmanubo@microsoft.com>
2020-02-14 12:08:56 +0100

    Change default output to AUX

--- gadget.js
+++ gadget.js
@@ -1,65 +1,63 @@
 ﻿var oShell = new ActiveXObject("WScript.Shell");
-var KefOut,KefHost
+var KefOut, KefHost
 
 System.Gadget.settingsUI = "settings.html";
 System.Gadget.onSettingsClosed = onSettingsClosed;
 
+System.Debug.outputString("Init\n");
 var lastTime = (new Date()).getTime();
 
-// power on every hour
-setInterval(KefOn,50*60*1000);
+// select output every 50 minutes (prevent lsx to enter standby after 1 hour)
+setInterval(KefOn, 50 * 60 * 1000);
 
 // power on when standby is detected
-setInterval(function() {
-  var currentTime = (new Date()).getTime();
-  //System.Debug.outputString("checking if no timer for more than 20s\n");
-  if (currentTime > (lastTime + 20*1000)) {  // ignore small delays
-    setTimeout(function() {
-     KefOn();
-    }, 0);
-  }
-  lastTime = currentTime;
-}, 10*1000);
+setInterval(function () {
+    var currentTime = (new Date()).getTime();
+    // System.Debug.outputString("currentTime="+currentTime + " lastTime="+lastTime+" diff="+(currentTime - lastTime)+"\n");
+    if (currentTime - lastTime > 20 * 1000) {
+        // give 10 more seconds for network to be up...etc
+        setTimeout(function () {
+            KefOn();
+        }, 10 * 1000);
+    }
+    lastTime = currentTime;
+}, 10 * 1000);
 
-
-function init()
-{    
-KefOut=System.Gadget.Settings.read("output");
-KefHost=System.Gadget.Settings.read("KefHost");
-if (KefOut=="") KefOut="Optical";
-if (KefHost=="") KefHost="192.168.0.58";
-KefOn();
+function init() {
+    KefOut = System.Gadget.Settings.read("output");
+    KefHost = System.Gadget.Settings.read("KefHost");
+    if (KefOut == "")
+        KefOut = "AUX";
+    if (KefHost == "")
+        KefHost = "192.168.0.58";
+    KefOn();
 }
 
-function onSettingsClosed()
-{
-init();
+function onSettingsClosed() {
+    init();
 }
 
-function terminate()
-{
+function terminate() {}
+
+function ExecCmd(cmd) {
+    var ExePath = "powershell.exe -File \"" + System.Gadget.path + "\\kefctrl.ps1\" ip:" + KefHost + " " + cmd;
+    // System.Debug.outputString(ExePath);
+    oShell.Run(ExePath, 0);
 }
 
-function ExecCmd(cmd)
-{
-var ExePath = "powershell.exe -File \"" + System.Gadget.path  + "\\kefctrl.ps1\" ip:" + KefHost + " " + cmd;
-//System.Debug.outputString(ExePath);
-//System.Debug.outputString("\n");
-oShell.Run(ExePath,0);
+function volumeDec() {
+    ExecCmd("dec");
 }
 
-function volumeDec()
-{
-ExecCmd("dec");
+function volumeInc() {
+    ExecCmd("inc");
 }
 
-function volumeInc()
-{
-ExecCmd("inc");
-}
-
-function KefOn()
-{
-System.Debug.outputString("KefOn()\n");
-if (KefOut=="Optical") {ExecCmd("opt");} else {ExecCmd("aux");}
+function KefOn() {
+    System.Debug.outputString("KefOn()\n");
+    if (KefOut == "Optical") {
+        ExecCmd("opt");
+    } else {
+        ExecCmd("aux");
+    }
 }
--- settings.js
+++ settings.js
@@ -1,31 +1,27 @@
 System.Gadget.onSettingsClosing = SettingsClosing;
 
-function onLoad()
-{
-loadSettings();
+function onLoad() {
+    loadSettings();
 }
 
-function onUnload()
-{
+function onUnload() {}
+
+function SettingsClosing(event) {
+    if (event.closeAction == event.Action.commit)
+        saveSettings();
+    event.cancel = false;
 }
 
-function SettingsClosing(event)
-{
-	if (event.closeAction == event.Action.commit) 
-		saveSettings();
-	event.cancel = false;
+function loadSettings() {
+    SelectOutput.value = System.Gadget.Settings.read("output");
+    KefHost.value = System.Gadget.Settings.read("KefHost");
+    if (SelectOutput.value == "")
+        SelectOutput.value = "AUX"
+	if (KefHost.value == "")
+		KefHost.value = "192.168.0.58"
 }
 
-function loadSettings()
-{
-    SelectOutput.value=System.Gadget.Settings.read("output");        	
-    KefHost.value=System.Gadget.Settings.read("KefHost"); 
-    if (SelectOutput.value=="") SelectOutput.value="Optical"
-    if (KefHost.value=="") KefHost.value="192.168.0.58"
-}
-
-function saveSettings()
-{
+function saveSettings() {
     System.Gadget.Settings.write("output", SelectOutput.value);
     System.Gadget.Settings.write("KefHost", KefHost.value);
 }
